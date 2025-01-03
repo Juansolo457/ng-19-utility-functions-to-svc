863b324fc54aad0212bad1c72c3ba010ef178267
Juansolo457 <isitluk@gmail.com>
2025-01-02 21:59:48 -0800

    working non service version

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,21 +1,34 @@
 // app.component.ts
 import { Component, OnInit } from '@angular/core';
+import { mapDictionaryToSelectOptions } from './map-util';
+import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule, NgFor],
   standalone: true, // <-- Important for standalone components
   template: `
     <h1>Hello from {{ name }}!</h1>
     <a target="_blank" href="https://angular.dev/overview">
       Learn more about Angular
     </a>
+    <select>
+    <option *ngFor="let opt of blah" [value]="opt.value">
+      {{ opt.label }}
+    </option>
+  </select>
+
   `,
 })
 export class AppComponent implements OnInit {
   name = 'Standalone Angular';
+  blah: any;
 
   ngOnInit(): void {
-    
+   this.blah = mapDictionaryToSelectOptions(userRoles); 
+   console.log('this', this.blah);
+   
   }
 }
 
